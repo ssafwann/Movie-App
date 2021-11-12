58ee5b43248abd00a94f9cf61d2b51ecd024492c
safwan917 <safwan917@gmail.com>
2021-11-13 00:27:17 +0800

    first commit

---
+++ app/src/main/java/com/example/movieapp/Login.java
@@ -0,0 +1,36 @@
+package com.example.movieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
+
+public class Login extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // button that sends to the sign up page
+        Button button = (Button) findViewById(R.id.signup_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.movieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 800);
+    }
+
+
+}
